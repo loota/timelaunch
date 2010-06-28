a85df5f2611a64da646e61a94f64cbfbb15acf7f
heikki <heikki@nassikka.pp.htv.fi>
2010-06-28 10:32:55 +0300

    Skeleton for the program

---
+++ dist/src/TimedSchedule.scala
@@ -0,0 +1,14 @@
+class TimedSchedule {
+    private var frequency = 0
+    private var duration = 0
+    private var launchAtStart = 0
+    private var launchAtEnd = 0
+
+    def getFrequency() {
+        return frequency
+    }
+    def getFrequency() {
+        return frequency
+    }
+
+}
